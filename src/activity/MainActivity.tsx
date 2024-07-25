e775c6b6c6491cc49cd49f54aea36d4aac7d6a04
hyunho4532 <kyounguk192@gmail.com>
2024-07-25 22:51:44 +0900

    Kakao Map Setting

--- src/activity/MainActivity.tsx
+++ src/activity/MainActivity.tsx
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
+
 export function MainActivity() {
+
+    useEffect(() => {
+        const container = document.getElementById('map');
+
+        if (container) {
+            const options = {
+                center: new kakao.maps.LatLng(33.450701, 126.570667),
+                level: 3
+            };
+
+            new kakao.maps.Map(container!, options);
+        }
+    });
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <div id="map" style={{ "width": "500px", "height": "400px" }}></div>
+        </>
     )
 }
